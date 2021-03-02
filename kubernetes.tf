02523d57ff9bce368e0452850b22400f1248ea43
DanyYanez <57074417+DanyYanez@users.noreply.github.com>
2021-03-02 10:04:29 -0500

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -33,7 +33,7 @@ resource "kubernetes_deployment" "flaskapp" {
       }
       spec {
         container {
-          image = "jenkinsci/blueocean"
+          image = "danyyanez/final_proyect"
           name  = "example"
 
           port {
